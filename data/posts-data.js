cac53ff44a4c292008acb136ee09ff411bbfb920
Jon <6867964@qq.com>
2017-09-22 16:22:32 +0800

    post

---
+++ data/posts-data.js
@@ -0,0 +1,24 @@
+var local_database = [
+  {
+    date: "Sep 22 2017",
+    imgSrc: "/images/post/3.png",
+    content: "北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔。",
+    reading: "269",
+    collect: "189",
+    comment: "569",
+    avatar: "/images/avatar.png"
+  },
+  {
+    date: "Sep 22 2017",
+    imgSrc: "/images/post/3.png",
+    content: "北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔。",
+    reading: "269",
+    collect: "189",
+    comment: "569",
+    avatar: "/images/avatar.png"
+  }
+]
+
+module.exports = {
+  postList: local_database
+}
---
+++ pages/welcome/welcome.js
@@ -0,0 +1,8 @@
+Page({
+    onTap:function()
+    {
+        wx.redirectTo({
+          url: '../posts/post',
+        })
+    }
+})
