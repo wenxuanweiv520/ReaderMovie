7a43e3d383e8279bfa95ca462c521f8916957109
Jon <6867964@qq.com>
2017-09-23 15:21:03 +0800

    post数据调用绑定

--- data/posts-data.js
+++ data/posts-data.js
@@ -10,7 +10,25 @@ var local_database = [
   },
   {
     date: "Sep 22 2017",
-    imgSrc: "/images/post/3.png",
+    imgSrc: "/images/post/1.png",
+    content: "北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔。",
+    reading: "269",
+    collect: "189",
+    comment: "569",
+    avatar: "/images/avatar.png"
+  },
+  {
+    date: "Sep 22 2017",
+    imgSrc: "/images/post/2.png",
+    content: "北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔。",
+    reading: "269",
+    collect: "189",
+    comment: "569",
+    avatar: "/images/avatar.png"
+  },
+  {
+    date: "Sep 22 2017",
+    imgSrc: "/images/post/1.png",
     content: "北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔。",
     reading: "269",
     collect: "189",
