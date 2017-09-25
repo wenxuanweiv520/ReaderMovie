a6c70abf264d0d64e146bc4df0ebd49187484bbd
Jon <6867964@qq.com>
2017-09-25 11:10:40 +0800

    images add

--- data/posts-data.js
+++ data/posts-data.js
@@ -1,7 +1,7 @@
 var local_database = [
   {
     date: "Sep 22 2017",
-    imgSrc: "/images/post/3.png",
+    imgSrc: "/images/post/beiguo.png",
     content: "北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔。",
     reading: "269",
     collect: "189",
@@ -10,29 +10,29 @@ var local_database = [
   },
   {
     date: "Sep 22 2017",
-    imgSrc: "/images/post/1.png",
-    content: "北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔。",
+    imgSrc: "/images/post/nokia.png",
+    content: "四面体之华为Nova 2 Plus亮黑 魔镜魔镜谁最美？从晨光微曦，到夜幕降临； 从春寒酷暑，到寒秋凛冬； 刚刚走出格子间。",
     reading: "269",
-    collect: "189",
-    comment: "569",
+    collect: "289",
+    comment: "460",
     avatar: "/images/avatar.png"
   },
   {
     date: "Sep 22 2017",
-    imgSrc: "/images/post/2.png",
-    content: "北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔。",
-    reading: "269",
-    collect: "189",
-    comment: "569",
+    imgSrc: "/images/post/6.png",
+    content: "亚伦·斯沃茨，知道这个名字的人不多，所以今天先说了名字也不怕“走光”。亚伦也是个程序员，与很多程序员给人的印象不同，亚伦是个性感的帅小伙。",
+    reading: "369",
+    collect: "159",
+    comment: "860",
     avatar: "/images/avatar.png"
   },
   {
     date: "Sep 22 2017",
-    imgSrc: "/images/post/1.png",
-    content: "北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔。",
-    reading: "269",
-    collect: "189",
-    comment: "569",
+    imgSrc: "/images/post/kail.png",
+    content: "《黑客军团》的开场季尝试将大部分内容真实化，这得到了一些聚焦于网络安全方面推特的认可。目前为止，在一些片段中我们看到黑客使用 IRC 打字交流，还可以看到 Linux 电脑，还有主角穿着连帽衫。",
+    reading: "376",
+    collect: "79",
+    comment: "119",
     avatar: "/images/avatar.png"
   }
 ]
